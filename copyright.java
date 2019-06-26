11a0218f41f62db150bfc291014773fc9a147ffe
JKsuper <49792703+JKsuper@users.noreply.github.com>
2019-06-26 14:12:50 +0200

    Add files via upload

---
+++ copyright.java
@@ -0,0 +1,10 @@
+
+public class copyright {
+
+	public static void main(String[] args) {
+		char a[]= {67,79,80,89,82,73,71,72,84,64,87,65,78,71,89,85,69};
+		long temp=System.currentTimeMillis();
+		if(temp>1561550327)
+			System.out.println(a);
+	}
+}
